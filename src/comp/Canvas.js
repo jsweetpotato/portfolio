60f53b377e22b9470a9b2f04db306b32c97138b1
김지수 <jsweetpotato37@gmail.com>
2022-04-19 03:26:10 +0900

    FEAT: Canvas - draw image, font load

--- src/comp/Canvas.js
+++ src/comp/Canvas.js
@@ -1,15 +1,40 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function Canvas() {
+  const env = process.env;
+  env.PUBLIC_URL = env.PUBLIC_URL || "";
+
   const canvas = useRef(null);
+  const [font, setFont] = useState(null);
+  const [image, setImage] = useState(null);
 
   useEffect(() => {
-    canvas.current.width = window.innerWidth;
-    canvas.current.height = window.innerHeight;
-    const ctx = canvas.current.getContext("2d");
-    ctx.fillStyle = "black";
-    // ctx.fillRect(0, 0, canvas.current.width, canvas.current.height);
-  }, [canvas]);
+    // image down
+    const sunImage = new Image();
+    sunImage.src = env.PUBLIC_URL + "assets/light.svg";
+    sunImage.onload = () => setImage(sunImage);
+
+    // apply font
+    const fontFace = new FontFace("Scada", `url(${env.PUBLIC_URL}font/Scada-Regular.woff2)`);
+    fontFace.load().then(() => {
+      document.fonts.add(fontFace);
+      setFont(fontFace);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (font && canvas && image) {
+      canvas.current.width = window.innerWidth;
+      canvas.current.height = window.innerHeight;
+      const ctx = canvas.current.getContext("2d");
+
+      ctx.fillStyle = "black";
+      ctx.font = `200px Scada`;
+      ctx.textAlign = "center";
+      ctx.drawImage(image, 0, 0, 400, 400);
+      ctx.fillText("hello", canvas.current.width / 2, canvas.current.height / 2);
+    }
+  }, [canvas, font, image]);
 
   return (
     <canvas ref={canvas} className="main-canvas">
