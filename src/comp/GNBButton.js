449891d9abf312bf05c52f61dedc77115efa195d
김지수 <jsweetpotato37@gmail.com>
2022-04-23 19:38:57 +0900

    gnb 버튼 스타일 변경으로 인한 코드 변경

--- src/comp/GNBButton.js
+++ src/comp/GNBButton.js
@@ -1,28 +1,12 @@
-import { useEffect, useState, useContext } from "react";
-import { DispatchContext } from "../App";
-
 const GNBButton = () => {
-  const { isMobile } = useContext(DispatchContext);
-  const [GNBButton, setGNBButton] = useState();
-
   const addClass = ({ target }) => {
-    target.closest("nav").classList.toggle("open");
+    target.closest("header").classList.toggle("open");
   };
 
-  useEffect(() => {
-    isMobile
-      ? setGNBButton(
-          <div className="hamburguer">
-            <span></span>
-          </div>
-        )
-      : setGNBButton(<p className="work">WORK</p>);
-  }, [isMobile]);
-
   return (
-    <button className="GNBBtn" onClick={addClass}>
-      {GNBButton}
-    </button>
+    <div className="hamburguer" onClick={addClass}>
+      <span></span>
+    </div>
   );
 };
 
