142f81413564dfab913f09252e3351ae84b614eb
김지수 <jsweetpotato37@gmail.com>
2022-04-29 01:32:34 +0900

    data.js

---
+++ src/data/data.js
@@ -0,0 +1,82 @@
+const data = {
+  responsive: [
+    {
+      id: 0,
+      title: "loruki",
+      image: "assets/loruki.png",
+      tag: ["Media Queries", "Vanila JS"],
+      url: "https://jsweetpotato.github.io/responsive-web-practice2.io/",
+    },
+    {
+      id: 1,
+      title: "space",
+      image: "assets/space.png",
+      tag: ["CSS Math Function", "Vanila JS"],
+      url: "jsweetpotato.github.io/spaceweb.io/",
+    },
+    {
+      id: 2,
+      title: "portfolio clonecode",
+      image: "assets/portfolio-clone.png",
+      tag: ["Media Queries", "Vanila JS"],
+      url: "https://jsweetpotato.github.io/responsive-web-practice4.io/",
+    },
+  ],
+  canvas: [
+    {
+      id: 0,
+      title: "interaction effect",
+      image: "assets/canvas1.png",
+      tag: ["Mouse Event", "Vanila JS"],
+      url: "https://jsweetpotato.github.io/canvas/EP1/index.html",
+    },
+    {
+      id: 1,
+      title: "bounce ball",
+      image: "assets/canvas2.png",
+      tag: ["Gravity", "Vanila JS"],
+      url: "https://jsweetpotato.github.io/canvas/EP2/index.html",
+    },
+    {
+      id: 2,
+      title: "collision",
+      image: "assets/canvas3.png",
+      tag: ["Collision", "Gravity", "Vanila JS"],
+      url: "https://jsweetpotato.github.io/canvas/bouncing/index.html",
+    },
+    {
+      id: 3,
+      title: "emoji particle",
+      image: "assets/canvas5.png",
+      tag: ["Mouse Event", "Vanila JS"],
+      url: "https://jsweetpotato.github.io/canvas/Particles&PhysicsEffect2/index.html",
+    },
+  ],
+  react: [
+    {
+      id: 0,
+      title: "emotion diary",
+      image: "assets/canvas5.png",
+      tag: ["Media Queries", "React Router"],
+      url: "https://jsweetpotato-emotion-diary.web.app/",
+    },
+  ],
+  ect: [
+    {
+      id: 0,
+      title: "cake",
+      image: "assets/cake.png",
+      tag: ["CSS3 3D Effect", "Vanila JS"],
+      url: "https://jsweetpotato.github.io/birthday.simulator.io/",
+    },
+    {
+      id: 1,
+      title: "horror face",
+      image: "assets/horror-face.png",
+      tag: ["Only Web", "Vanila JS"],
+      url: "https://jsweetpotato.github.io/horrorface.io/",
+    },
+  ],
+};
+
+export { data };
