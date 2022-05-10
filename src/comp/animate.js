cddb5afcbfa177728cc43a93461f6bd3fbba01df
김지수 <jsweetpotato37@gmail.com>
2022-05-10 21:59:44 +0900

    STYLE: chnage animation

--- src/comp/animate.js
+++ src/comp/animate.js
@@ -1,26 +1,35 @@
 export const animate = (target, value) => {
-  target.animate([
+  target.animate(
+    [
+      {
+        height: 0,
+        opacity: 0,
+      },
+      {
+        height: value + "px",
+        opacity: 1,
+      },
+    ],
     {
-      height: 0
-    },
-    {
-      height: value + "px"
+      duration: 600,
+      delay: 250,
+      easing: "cubic-bezier(0.19, 1, 0.22, 1)",
     }
-  ], {
-    duration: 600,
-    easing: "cubic-bezier(0.19, 1, 0.22, 1)"
-  })
-}
+  );
+};
 
 export const animate2 = (target, value) => {
-  target.animate([
+  target.animate(
+    [
+      {
+        height: value + "px",
+      },
+      {
+        height: 0,
+      },
+    ],
     {
-      height: value + "px"
-    },
-    {
-      height: 0
+      duration: 500,
     }
-  ], {
-    duration: 500
-  })
-}
+  );
+};
